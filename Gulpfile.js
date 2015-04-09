4583d89cadf52294fd087f84f336beaa34bdd74f
DBSQA <dbsqa@DBSQAs-MacBook-Pro.local>
2015-04-10 00:05:54 +0800

    Live reload added through BrowserSync

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp');
-var sourcemaps = require('gulp-sourcemaps');
 var traceur = require('gulp-traceur');
 var concat = require('gulp-concat');
-var connect = require("gulp-connect");
 var jshint = require('gulp-jshint');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
 var SOURCE_FILES = ['!app/**/*_test.js', 'app/*.js', 'app/modules/**/*.js'];
+var SOURCE_HTML = ['app/*.html', 'app/modules/**/*.html'];
 
 // Runs JSHint Report against all JS files in app
 gulp.task('lint', function () {
@@ -15,32 +16,35 @@ gulp.task('lint', function () {
         .pipe(jshint.reporter('fail'));
 });
 
+gulp.task('browserSync', function() {
+    browserSync({
+        logConnections: true,
+        logFileChanges: true,
+        notify: true,
+        open: true,
+        server: {
+            baseDir: "./app"
+        }
+    });
+});
+
 gulp.task('watch', function () {
 
     // Lint the JS files when they change
-    gulp.watch(SOURCE_FILES, ['lint', 'traceur']);
+    gulp.watch(SOURCE_FILES, ['lint', 'traceur', reload]);
+    gulp.watch(SOURCE_HTML, reload);
 });
 
 /* Sourcemaps seem to not be working when a base is specified */
 gulp.task('traceur', function () {
     return gulp.src(['!app/**/*_test.js', 'app/*.js', 'app/modules/**/*.js'], {base: './app'})
-        //.pipe(sourcemaps.init())
         .pipe(traceur({
             modules: 'register',
             moduleName : true
         }))
         .pipe(concat('bundle.js'))
-        //.pipe(sourcemaps.write())
-        .pipe(gulp.dest('app/dist'));
+        .pipe(gulp.dest('app/dist'))
+        .pipe(reload({stream:true}));
 });
 
-gulp.task('connect', function () {
-
-    // Uses gulp-connect plugin to start up a server
-    connect.server({
-        root: ['app'],
-        port: 9000
-    });
-});
-
-gulp.task('default', ['traceur', 'lint', 'watch', 'connect']);
+gulp.task('default', ['traceur', 'lint', 'watch', 'browserSync']);
